a2a4ef047a49735004e12353965d677a12b3b853
cgq <126@qq.com>
2022-05-13 10:24:28 +0800

    first commit

---
+++ src/main/java/com/atguigu/git/demo.java
@@ -0,0 +1,11 @@
+package com.atguigu.git;
+
+/**
+ * @author cgq
+ * @create 2022-05-13 0:46
+ */
+public class demo {
+    public static void main(String[] args) {
+        System.out.println("hello");
+    }
+}
