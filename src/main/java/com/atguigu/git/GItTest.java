0b641e6ba8633bdf9682446f9aefcf8bf0ea2c4f
cgq <126@qq.com>
2022-05-13 10:32:47 +0800

    hot-fix commit

--- src/main/java/com/atguigu/git/GItTest.java
+++ src/main/java/com/atguigu/git/GItTest.java
@@ -8,5 +8,6 @@ public class GItTest {
         System.out.println("hello git!");
         System.out.println("hello git2!");
         System.out.println("second commit");
+        System.out.println("hot-fix commit");
     }
 }
